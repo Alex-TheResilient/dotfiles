54c8e96ab0ea8b6bb37ae407d09a81f90a3f49e2
atipaq <alex.ronny.hq@gmail.com>
2025-04-06 15:07:55 -0500

    fix(nvim): add oil plugin

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
+-- This file contains custom key mappings for Neovim.
+
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+----- OIL -----
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,20 @@
+-- This file  contains the configuration for the oil.nvim plugin in Neovim
+
+return {
+  -- Plugin: oil.nvim
+  -- URL: https://github.com/stevearc/oil.nvim
+  -- Description: A Neovim plugin for managing and navigating directories.
+  "stevearc/oil.nvim",
+
+  opts = {
+    -- Key mappings for oil.nvim actions
+  },
+
+  -- Optional dependencies
+  dependencies = {
+    -- Plugin: nvim-web-devicons
+    -- URL: https://github.com/nvim-tree/nvim-web-devicons
+    -- Description: A Lua fork of vim-web-devicons for Neovim.
+    "nvim-tree/nvim-web-devicons",
+  },
+}
