ded7a2e8d6cfd020404459b1507734c914d100ce
atipaq <alex.ronny.hq@gmail.com>
2025-04-06 15:06:52 -0500

    fix(nvim): add module colorscheme

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    -- LazyVim configuration
+    "LazyVim/LazyVim",
+    opts = {
+      -- Set the default color scheme
+      colorscheme = "tokyonight",
+    },
+  },
+}
