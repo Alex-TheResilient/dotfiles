a5f73eb93495bdcfb06749c59332f575bb78a4ae
atipaq <alex.ronny.hq@gmail.com>
2025-04-11 20:44:01 -0500

    feat: Add ghosty

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo "Setting up nvim with with LazyVim"
-mkdir -p ~/.config
+echo "Setting up nvim with LazyVim"
+rm -rf ~/.config/nvim
 ln -s ~/dotfiles/nvim ~/.config/nvim
+echo "✅ Nvim (LazyVim) successfully linked"
 
-echo "Nvim (lazyVim) successfully linked"
+echo "Setting up Ghostty config"
+rm -rf ~/.config/ghostty
+ln -s ~/dotfiles/ghostty ~/.config/ghostty
+echo "✅ Ghostty config successfully linked"
