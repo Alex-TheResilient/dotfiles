311edb0c73fb2a70af0b0fab3de712d67bd80b88
Alex Huaracha <129701149+Alex-TheResilient@users.noreply.github.com>
2025-08-01 14:49:31 -0500

    refactor: remove Neovim and Ghostty configuration

--- nvim/lua/config/keymaps.lua
+++
@@ -1,8 +0,0 @@
--- This file contains custom key mappings for Neovim.
-
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
------ OIL -----
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
